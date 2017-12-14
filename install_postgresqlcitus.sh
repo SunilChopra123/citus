d0fa3af37ee112d0c5e967f20883b539839e866a
SunilChopra123 <34117139+SunilChopra123@users.noreply.github.com>
2017-12-14 00:09:57 -0800

    Update install_postgresqlcitus.sh

--- install_postgresqlcitus.sh
+++ install_postgresqlcitus.sh
@@ -1,24 +1,6 @@
-# You must be root to run this script
-if [ "${UID}" -ne 0 ];
-then
-    log "Script executed without root permissions"
-    echo "You must be root to run this program." >&2
-    exit 3
-fi
-
-# TEMP FIX - Re-evaluate and remove when possible
-# This is an interim fix for hostname resolution in current VM (If it does not exist add it)
 grep -q "${HOSTNAME}" /etc/hosts
-if [ $? == 0 ];
-then
-  echo "${HOSTNAME}found in /etc/hosts"
-else
-  echo "${HOSTNAME} not found in /etc/hosts"
-  # Append it to the hsots file if not there
-  echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
-fi
+echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
 
-#!/bin/bash
 curl https://install.citusdata.com/community/deb.sh | sudo bash
 sudo apt-get -y install postgresql-9.6-citus-7.0
 sudo pg_conftool 9.6 main set shared_preload_libraries citus
