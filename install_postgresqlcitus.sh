5736710043a0375155bf2deb7102a198609ac595
SunilChopra123 <34117139+SunilChopra123@users.noreply.github.com>
2017-12-19 17:45:13 -0800

    Update install_postgresqlcitus.sh

--- install_postgresqlcitus.sh
+++ install_postgresqlcitus.sh
@@ -1,11 +1,12 @@
-grep -q "${HOSTNAME}" /etc/hosts
-echo "127.0.0.1 ${HOSTNAME}" >> /etc/hosts
-
+#!/bin/bash
 curl https://install.citusdata.com/community/deb.sh | sudo bash
-sudo apt-get -y install postgresql-9.6-citus-7.0
-sudo pg_conftool 9.6 main set shared_preload_libraries citus
-sudo pg_conftool 9.6 main set listen_addresses '*'
-sudo cat > /etc/postgresql/9.6/main/pg_hba.conf <<DELIM
+sudo apt-get -y install postgresql-10-citus-7.1
+sudo pg_conftool 10 main set shared_preload_libraries citus
+sudo pg_conftool 10 main set listen_addresses '*'
+
+sudo chmod 755 /etc/postgresql/10/main/pg_hba.conf 
+sudo chown azureuser /etc/postgresql/10/main/pg_hba.conf 
+sudo cat > /etc/postgresql/10/main/pg_hba.conf <<DELIM
 
 # Allow unrestricted access to nodes in the local network. The following ranges
 # correspond to 24, 20, and 16-bit blocks in Private IPv4 address spaces.
@@ -17,4 +18,3 @@ host    all             all             ::1/128                 trust
 DELIM
 sudo service postgresql restart
 sudo update-rc.d postgresql enable
-sudo -i -u postgres psql -c "CREATE EXTENSION citus;"
